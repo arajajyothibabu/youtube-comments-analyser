c3a08fde352e45b7a27dad05797aceeb0c6628d4
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2020-11-30 16:23:34 +0530

    implemented search filters

--- content-script.js
+++ content-script.js
@@ -2,7 +2,6 @@
   var COMMENTS = "ytd-comments";
   var COMMENT_THREAD = "ytd-comment-thread-renderer";
   var COMMENT = "ytd-comment-renderer";
-  var EXPANDER = "ytd-expander";
   var COMMENT_REPLIES = "ytd-comment-replies-renderer";
 
   var REPLIES_BUTTON = "ytd-button-renderer#more-replies";
@@ -15,28 +14,72 @@
     ":not([hidden])";
   var PAPER_BUTTON = "paper-button";
 
-  var FIRST_COMMENT = COMMENTS + "#comments " + COMMENT_THREAD;
+  var FIRST_COMMENT =
+    COMMENTS + "#comments:not([hidden]) " + COMMENT_THREAD + ":not([hidden])";
   var APP = "ytd-app";
 
   var APPLY = "Apply";
   var LOAD_ALL = "Load All";
 
+  var YCA_FORM = "yca-form";
+  var YCA_DATA = "yca-data";
+  var YCA_OVERLAY = "yca-overlay";
+
+  var FORCE_STOP_LOADING = false;
+
   var appEl;
   var info = {};
   var commentsEl;
 
-  function getCommentNodes(fromIndex) {
-    return Array.from(commentsEl.querySelectorAll(COMMENT_THREAD)).slice(
-      fromIndex || 0
-    );
+  function filterMatched(el, filters) {
+    var authorMatched = false;
+    var searchMatched = false;
+    if (filters.author) {
+      authorMatched = el
+        .querySelector("a#author-text")
+        .textContent.toLowerCase()
+        .includes(filters.author);
+    }
+    if (filters.searchRegex) {
+      filters.searchRegex.lastIndex = 0;
+      var textContent = "";
+      el.querySelectorAll("a#author-text,#content-text").forEach(function (el) {
+        textContent += " " + el.textContent.trim();
+      });
+      searchMatched = filters.searchRegex.test(textContent.toLowerCase());
+    }
+    if (filters.author && filters.searchRegex) {
+      if (authorMatched && searchMatched) {
+        return true;
+      }
+    } else if (filters.author) {
+      if (authorMatched) {
+        return true;
+      }
+    } else if (filters.searchRegex) {
+      if (searchMatched) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function getCommentNodes(fromIndex, filters) {
+    var nodes = [];
+    commentsEl.querySelectorAll(COMMENT_THREAD).forEach(function (el) {
+      applyFilters(el, nodes, filters);
+    });
+    return nodes.slice(fromIndex || 0);
   }
 
   var formEl;
+  var overlay;
 
   var form = {
-    button: null,
-    input: null,
+    apply: null,
+    search: null,
     author: null,
+    loadAll: null,
   };
 
   function waitToLoad(fn, queryFn) {
@@ -61,30 +104,51 @@
     var obj = {};
     try {
       obj.author = getText(el.querySelector("a#author-text"));
-      obj.content = getText(el.querySelector(EXPANDER + " " + "#content-text"));
+      obj.content = getText(el.querySelector("#content-text"));
     } catch (e) {}
     return obj;
+  }
+
+  function applyFilters(el, nodes, filters) {
+    el.hidden = false;
+    if (filters) {
+      if (filterMatched(el, filters)) {
+        nodes.push(el);
+      } else {
+        el.hidden = true;
+      }
+    } else {
+      nodes.push(el);
+    }
+  }
+
+  function getReplyNodes(el, filters) {
+    var nodes = [];
+    el.querySelectorAll(
+      COMMENT_REPLIES + " " + "#loaded-replies" + ">" + COMMENT
+    ).forEach(function (el) {
+      applyFilters(el, nodes, filters);
+    });
+    return nodes;
   }
 
   /**
    *
    * @param {*} el <ytd-comment-thread-renderer />
    */
-  function getCommentThread(el) {
+  function getCommentThread(el, filters) {
     var obj = {};
     try {
       obj = getComment(el.querySelector(COMMENT + "#comment"));
-      obj.replies = Array.from(
-        el.querySelectorAll(
-          COMMENT_REPLIES + " " + "#loaded-replies" + ">" + COMMENT
-        )
-      ).map(getComment);
+      obj.replies = getReplyNodes(el, filters).map(getComment);
     } catch (e) {}
     return obj;
   }
 
-  function getComments(fromIndex) {
-    return getCommentNodes(0).map(getCommentThread);
+  function getComments(fromIndex, filters) {
+    return getCommentNodes(fromIndex, filters).map(function (el) {
+      return getCommentThread(el, filters);
+    });
   }
 
   function getInfo() {
@@ -119,16 +183,19 @@
   function loadComments() {
     window.scrollTo(0, appEl.scrollHeight);
     loadReplies();
-    if (!isEverythingLoaded()) {
+    if (!isEverythingLoaded() && !FORCE_STOP_LOADING) {
       setTimeout(loadComments, 100);
     } else {
-      form.button.disabled = false;
+      if (FORCE_STOP_LOADING) {
+        form.loadAll.disabled = true;
+      }
+      overlay.hidden = true;
     }
   }
 
-  function getData() {
+  function getData(filters) {
     var obj = Object.assign({}, info);
-    obj.comments = getComments(0);
+    obj.comments = getComments(0, filters);
     return obj;
   }
 
@@ -139,10 +206,22 @@
   function attachForm() {
     var firstEl = commentsEl.querySelector(FIRST_COMMENT);
     formEl = document.createElement("form");
-    formEl.classList.add("yca");
+    formEl.classList.add(YCA_FORM);
     formEl.style =
       makeStyle(100) + "justify-content: space-between; margin-bottom: 16px;";
     firstEl.parentElement.insertBefore(formEl, firstEl);
+
+    button = document.createElement("button");
+    button.innerText = LOAD_ALL;
+    button.style = "width: 12%";
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      overlay.hidden = false;
+      loadComments();
+      e.target.disabled = true;
+    });
+    form.loadAll = button;
+    formEl.appendChild(button);
 
     var authorInput = document.createElement("input");
     authorInput.style = makeStyle(24);
@@ -151,50 +230,111 @@
     form.author = authorInput;
 
     var keyworkInput = document.createElement("textarea");
-    keyworkInput.style = makeStyle(54);
+    keyworkInput.style = makeStyle(44);
     keyworkInput.placeholder = "Search: from hyderabad, india, email";
     formEl.appendChild(keyworkInput);
-    form.input = keyworkInput;
+    form.search = keyworkInput;
 
     button = document.createElement("button");
-    button.innerText = LOAD_ALL;
-    button.style = "width: 14%";
+    button.innerText = APPLY;
+    button.style = "width: 12%";
     button.addEventListener("click", function (e) {
       e.preventDefault();
-      if (e.target.innerText === LOAD_ALL) {
-        e.target.disabled = true;
-        e.target.innerText = APPLY;
-        loadComments();
-      } else {
-        attachData();
-      }
+      fetchData();
     });
-    form.button = button;
+    form.apply = button;
     formEl.appendChild(button);
+
+    /**
+     * Overlay
+     */
+    overlay = document.createElement("div");
+    overlay.style =
+      "width: 100%; height: 100%; position:fixed;" +
+      "left:0;right:0;top:0;bottom:0;z-index: 100000001;" +
+      "background:rgba(33, 33, 33, 0.9);text-align: center;";
+    overlay.classList.add(YCA_OVERLAY);
+    overlay.hidden = true;
+
+    var overlayContent = document.createElement("div");
+    overlayContent.appendChild(document.createTextNode("Loading comments..."));
+    overlayContent.appendChild(document.createElement("br"));
+    overlayContent.appendChild(
+      document.createTextNode("If you feel it's talking longer, ")
+    );
+    overlayContent.style = "color: #F1F1F1; padding-top: 40vh;font-size: 18px;";
+
+    var cancelButton = document.createElement("a");
+    overlayContent.appendChild(cancelButton);
+    cancelButton.href = "javascript:void();";
+    cancelButton.innerText = "click here";
+    cancelButton.addEventListener("click", function (el) {
+      FORCE_STOP_LOADING = true;
+    });
+
+    overlayContent.appendChild(document.createTextNode(" to cancel."));
+    overlay.appendChild(overlayContent);
+    formEl.appendChild(overlay);
+    /**
+     * END: Overlay
+     */
   }
 
-  function attachData() {
-    var data = getData();
+  function escapeRegExp(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+  }
+
+  function getAppliedFilters() {
+    var author = form.author.value;
+    var search = form.search.value;
+    var obj = {};
+    if (author && author.trim()) {
+      obj.author = author.trim().toLowerCase();
+    }
+    if (search && search.trim()) {
+      try {
+        var keywords = search
+          .trim()
+          .split(" ")
+          .filter((_) => _.trim())
+          .map((r) => escapeRegExp(r));
+        var regex = new RegExp(keywords.join("|"));
+        obj.searchRegex = regex;
+      } catch (e) {}
+    }
+    console.log(obj);
+    return obj.author || obj.searchRegex ? obj : null;
+  }
+
+  function fetchData() {
+    var data = getData(getAppliedFilters());
+    removeIfExisting("div." + YCA_DATA);
     var pre = document.createElement("pre");
     pre.innerHTML = JSON.stringify(data, null, 2);
     pre.id = "content-text";
+    pre.contentEditable = true;
     pre.classList.add("ytd-comment-renderer");
     pre.style = "border: 1px solid #CCCCCC";
     var wrapper = document.createElement("div");
+    wrapper.classList.add(YCA_DATA);
     wrapper.style = "max-height: 100vh; overflow-y:auto;margin: 16px;";
     wrapper.appendChild(pre);
     var firstEl = commentsEl.querySelector(FIRST_COMMENT);
     firstEl.parentElement.insertBefore(wrapper, firstEl);
   }
 
+  function removeIfExisting(query) {
+    var existing = commentsEl.querySelector(query);
+    if (existing) {
+      existing.parentElement.removeChild(existing);
+    }
+  }
+
   function _init() {
     appEl = document.querySelector(APP);
     commentsEl = appEl.querySelector(COMMENTS);
     info = getInfo();
-    var existing = commentsEl.querySelector("form.yca");
-    if (existing) {
-      existing.parentElement.removeChild(existing);
-    }
+    removeIfExisting("form." + YCA_FORM);
     attachForm();
   }
 
